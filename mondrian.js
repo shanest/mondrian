e6d09f5a10313513b06542b047af17f0a7058138
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2022-01-31 16:51:14 -0800

    basic formatting of table in

--- mondrian.js
+++ mondrian.js
@@ -8,7 +8,7 @@ https://www.nltk.org/_modules/nltk/tree.html
 const escapeRegExp = function escapeRegExp(string) {
     return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
 };
-const parse_string = function parse_string(string, open_bracket = "(", close_bracket = ")") {
+const parseString = function parse_string(string, open_bracket = "(", close_bracket = ")") {
     // TODO: docstring here
     // TODO: error handling
     // prep reg-ex for tokenizing the linearized parse tree
@@ -43,4 +43,56 @@ const parse_string = function parse_string(string, open_bracket = "(", close_bra
     }
     const tree = stack[stack.length - 1].children[0];
     return tree;
+};
+const treeToTable = function (tree, verticalProb = 0.5) {
+    // TODO: docstring
+    // TODO: style it!!
+    // TODO: heights/widths and/or colors somehow derived from length of label??
+    // base case: no table for leaf nodes
+    if (tree.children.length == 0) {
+        return null;
+    }
+    let table = document.createElement('table');
+    table.style.padding = "0";
+    table.style.borderCollapse = "collapse";
+    const vertical_split = Math.random() < verticalProb;
+    // add parent tr if going to split into td's, else nothing
+    let parent_node;
+    if (vertical_split) {
+        parent_node = document.createElement('tr');
+        table.appendChild(parent_node);
+    }
+    else {
+        parent_node = table;
+    }
+    for (const child of tree.children) {
+        let child_node;
+        if (vertical_split) {
+            child_node = document.createElement('td');
+            parent_node.appendChild(child_node);
+        }
+        else {
+            let intermediate_node = document.createElement('tr');
+            child_node = document.createElement('td');
+            intermediate_node.appendChild(child_node);
+            parent_node.appendChild(intermediate_node);
+        }
+        const child_table = treeToTable(child);
+        if (child_table != null) {
+            child_node.appendChild(child_table);
+            // turn borders off if there will be a nested table
+            child_node.style.border = "0";
+        }
+    }
+    return table;
+};
+window.onload = function () {
+    const testString = "(S (NP (Det The) (N Teacher)) (VP (V talks) (Adv quickly)))";
+    let frame = document.getElementById("frame");
+    if (frame != null) {
+        const table = treeToTable(parseString(testString));
+        if (table != null) {
+            frame.appendChild(table);
+        }
+    }
 };
--- mondrian.ts
+++ mondrian.ts
@@ -14,7 +14,7 @@ interface Tree {
     children: Array<Tree>;
 }
 
-const parse_string = function parse_string(string: string, open_bracket: string = "(", close_bracket: string = ")"): Tree {
+const parseString = function parse_string(string: string, open_bracket: string = "(", close_bracket: string = ")"): Tree {
     // TODO: docstring here
     // TODO: error handling
     // prep reg-ex for tokenizing the linearized parse tree
@@ -49,3 +49,55 @@ const parse_string = function parse_string(string: string, open_bracket: string 
     const tree: Tree = stack[stack.length - 1].children[0];
     return tree;
 }
+
+const treeToTable = function (tree: Tree, verticalProb: number = 0.5): HTMLTableElement | null {
+    // TODO: docstring
+    // TODO: style it!!
+    // TODO: heights/widths and/or colors somehow derived from length of label??
+    // base case: no table for leaf nodes
+    if (tree.children.length == 0) {
+        return null;
+    }
+    let table: HTMLTableElement = document.createElement('table');
+    table.style.padding = "0";
+    table.style.borderCollapse = "collapse";
+    const vertical_split: boolean = Math.random() < verticalProb;
+    // add parent tr if going to split into td's, else nothing
+    let parent_node: HTMLElement;
+    if (vertical_split) {
+        parent_node = document.createElement('tr');
+        table.appendChild(parent_node);
+    } else {
+        parent_node = table;
+    }
+    for (const child of tree.children) {
+        let child_node: HTMLTableCellElement;
+        if (vertical_split) {
+            child_node = document.createElement('td');
+            parent_node.appendChild(child_node);
+        } else {
+            let intermediate_node: HTMLElement = document.createElement('tr');
+            child_node = document.createElement('td');
+            intermediate_node.appendChild(child_node);
+            parent_node.appendChild(intermediate_node);
+        }
+        const child_table = treeToTable(child);
+        if (child_table != null) {
+            child_node.appendChild(child_table);
+            // turn borders off if there will be a nested table
+            child_node.style.border = "0";
+        }
+    }
+    return table;
+}
+
+window.onload = function () {
+    const testString: string = "(S (NP (Det The) (N Teacher)) (VP (V talks) (Adv quickly)))";
+    let frame = document.getElementById("frame");
+    if (frame != null) {
+        const table = treeToTable(parseString(testString));
+        if (table != null) {
+            frame.appendChild(table);
+        }
+    }
+};
